5f24546778e225984ab6070ef3942a5d4c2f1c45
esanmiguelc <esanmiguel@gmail.com>
2015-07-29 07:37:07 -0500

    Add active record spec helper

---
+++ spec/active_record_spec_helper.rb
@@ -0,0 +1,14 @@
+require 'active_record'
+require 'yaml'
+
+connection_info = YAML.load_file('config/database.yml')['test']
+ActiveRecord::Base.establish_connection(connection_info)
+
+RSpec.configure do |config|
+  config.around do |example|
+    ActiveRecord::Base.transaction do
+      example.run
+      raise ActiveRecord::Rollback
+    end
+  end
+end
